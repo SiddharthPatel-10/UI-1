610d491f45ba3cf7a8b3e3999904b87e31b45907
siddharth patel <connectwithsiddh@gmail.com>
2024-05-22 22:54:58 +0530

    feat: Add featured section with stunning UI and effects; create button in card section (page9)
    - Implemented the featured section with an impressive design.
    - Added visually appealing animations and effects to enhance user experience.
    - Created a button within the card section on page9 for improved interaction.
    
    Note: The featured section UI and effects are still a work in progress.

--- index.js
+++ index.js
@@ -243,10 +243,88 @@ function faq() {
   });
 }
 
+function featured(){
+console.log("hello");
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
 
+Shery.hoverWithMediaCircle(".hvr" /* Element to target.*/, {
+  // images: ["image1.jpg", "image2.jpg", "image3.jpg"] /*OR*/,
+  videos: ["video1.mp4", "video2.mp4"],
+});
+
+
+gsap.to(".featured_left_elem", {
+    scrollTrigger: {
+      trigger: "#featuredImages",
+      pin: true,
+      start: "top top",
+      end: "bottom bottom",
+      endTrigger: ".last",
+      scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1,
+  });
+  
+
+let sections = document.querySelectorAll(".featured_left_elem");
+Shery.imageEffect(".images", {
+  style: 5,
+  // config: {onMouse, {value: 1}},
+  slideStyle: (setScroll) => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY / innerHeight); //Updating the scroll
+    });
+  },
+});
+
+
+}
 locomotiveAnimation();
 navAnimation();
 page2Animation();
 page3VideoAnimation();
 // turnOffInspect()
 faq();
+featured()
+function fea(){
+  Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+});
+
+gsap.to(".featured_left_elem", {
+  scrollTrigger: {
+    trigger: "#featuredImages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".featured_left_elem");
+Shery.imageEffect(".images", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
+}
+
+// fea()
